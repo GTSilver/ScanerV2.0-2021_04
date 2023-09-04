0af54ec5b279b9a9821ae132628df0e350b62d61
GTSilver <gtsilver.work@gmail.com>
2023-09-04 17:43:24 +0500

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Scaner_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input_TB = new System.Windows.Forms.TextBox();
+            this.Output_TB = new System.Windows.Forms.TextBox();
+            this.Start_B = new System.Windows.Forms.Button();
+            this.BrowseInput_B = new System.Windows.Forms.Button();
+            this.BrowseOutput_B = new System.Windows.Forms.Button();
+            this.Separator_RB3 = new System.Windows.Forms.RadioButton();
+            this.Separator_RB4 = new System.Windows.Forms.RadioButton();
+            this.Separator_RB5 = new System.Windows.Forms.RadioButton();
+            this.Separator_RB6 = new System.Windows.Forms.RadioButton();
+            this.Separator_RB1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Separator_RB2 = new System.Windows.Forms.RadioButton();
+            this.Stop_B = new System.Windows.Forms.Button();
+            this.Desine_L1 = new System.Windows.Forms.Label();
+            this.Desine_L2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Size_L = new System.Windows.Forms.Label();
+            this.Folders_L = new System.Windows.Forms.Label();
+            this.Files_L = new System.Windows.Forms.Label();
+            this.Desine_L6 = new System.Windows.Forms.Label();
+            this.Desine_L3 = new System.Windows.Forms.Label();
+            this.Desine_L4 = new System.Windows.Forms.Label();
+            this.Desine_L7 = new System.Windows.Forms.Label();
+            this.Time_L = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Input_TB
+            // 
+            this.Input_TB.Location = new System.Drawing.Point(84, 6);
+            this.Input_TB.Name = "Input_TB";
+            this.Input_TB.Size = new System.Drawing.Size(168, 20);
+            this.Input_TB.TabIndex = 0;
+            this.Input_TB.Text = "C:\\";
+            this.Input_TB.TextChanged += new System.EventHandler(this.Input_TB_TextChanged);
+            // 
+            // Output_TB
+            // 
+            this.Output_TB.Location = new System.Drawing.Point(84, 32);
+            this.Output_TB.Name = "Output_TB";
+            this.Output_TB.Size = new System.Drawing.Size(168, 20);
+            this.Output_TB.TabIndex = 1;
+            this.Output_TB.Text = "D:\\";
+            this.Output_TB.TextChanged += new System.EventHandler(this.Output_TB_TextChanged);
+            // 
+            // Start_B
+            // 
+            this.Start_B.Location = new System.Drawing.Point(206, 58);
+            this.Start_B.Name = "Start_B";
+            this.Start_B.Size = new System.Drawing.Size(127, 23);
+            this.Start_B.TabIndex = 2;
+            this.Start_B.Text = "Start";
+            this.Start_B.UseVisualStyleBackColor = true;
+            this.Start_B.Click += new System.EventHandler(this.Start_B_Click);
+            // 
+            // BrowseInput_B
+            // 
+            this.BrowseInput_B.Location = new System.Drawing.Point(258, 4);
+            this.BrowseInput_B.Name = "BrowseInput_B";
+            this.BrowseInput_B.Size = new System.Drawing.Size(75, 23);
+            this.BrowseInput_B.TabIndex = 4;
+            this.BrowseInput_B.Text = "Open";
+            this.BrowseInput_B.UseVisualStyleBackColor = true;
+            this.BrowseInput_B.Click += new System.EventHandler(this.BrowseInput_B_Click);
+            // 
+            // BrowseOutput_B
+            // 
+            this.BrowseOutput_B.Location = new System.Drawing.Point(258, 30);
+            this.BrowseOutput_B.Name = "BrowseOutput_B";
+            this.BrowseOutput_B.Size = new System.Drawing.Size(75, 23);
+            this.BrowseOutput_B.TabIndex = 5;
+            this.BrowseOutput_B.Text = "Open";
+            this.BrowseOutput_B.UseVisualStyleBackColor = true;
+            this.BrowseOutput_B.Click += new System.EventHandler(this.BrowseOutput_B_Click);
+            // 
+            // Separator_RB3
+            // 
+            this.Separator_RB3.AutoSize = true;
+            this.Separator_RB3.Location = new System.Drawing.Point(9, 31);
+            this.Separator_RB3.Name = "Separator_RB3";
+            this.Separator_RB3.Size = new System.Drawing.Size(29, 17);
+            this.Separator_RB3.TabIndex = 7;
+            this.Separator_RB3.Text = "*";
+            this.Separator_RB3.UseVisualStyleBackColor = true;
+            // 
+            // Separator_RB4
+            // 
+            this.Separator_RB4.AutoSize = true;
+            this.Separator_RB4.Location = new System.Drawing.Point(39, 31);
+            this.Separator_RB4.Name = "Separator_RB4";
+            this.Separator_RB4.Size = new System.Drawing.Size(30, 17);
+            this.Separator_RB4.TabIndex = 9;
+            this.Separator_RB4.Text = "\"";
+            this.Separator_RB4.UseVisualStyleBackColor = true;
+            // 
+            // Separator_RB5
+            // 
+            this.Separator_RB5.AutoSize = true;
+            this.Separator_RB5.Location = new System.Drawing.Point(9, 49);
+            this.Separator_RB5.Name = "Separator_RB5";
+            this.Separator_RB5.Size = new System.Drawing.Size(31, 17);
+            this.Separator_RB5.TabIndex = 10;
+            this.Separator_RB5.Text = "<";
+            this.Separator_RB5.UseVisualStyleBackColor = true;
+            // 
+            // Separator_RB6
+            // 
+            this.Separator_RB6.AutoSize = true;
+            this.Separator_RB6.Location = new System.Drawing.Point(39, 49);
+            this.Separator_RB6.Name = "Separator_RB6";
+            this.Separator_RB6.Size = new System.Drawing.Size(31, 17);
+            this.Separator_RB6.TabIndex = 11;
+            this.Separator_RB6.Text = ">";
+            this.Separator_RB6.UseVisualStyleBackColor = true;
+            // 
+            // Separator_RB1
+            // 
+            this.Separator_RB1.AutoSize = true;
+            this.Separator_RB1.Checked = true;
+            this.Separator_RB1.Location = new System.Drawing.Point(9, 13);
+            this.Separator_RB1.Name = "Separator_RB1";
+            this.Separator_RB1.Size = new System.Drawing.Size(32, 17);
+            this.Separator_RB1.TabIndex = 12;
+            this.Separator_RB1.TabStop = true;
+            this.Separator_RB1.Text = "T";
+            this.Separator_RB1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Separator_RB2);
+            this.groupBox1.Controls.Add(this.Separator_RB6);
+            this.groupBox1.Controls.Add(this.Separator_RB1);
+            this.groupBox1.Controls.Add(this.Separator_RB3);
+            this.groupBox1.Controls.Add(this.Separator_RB4);
+            this.groupBox1.Controls.Add(this.Separator_RB5);
+            this.groupBox1.Location = new System.Drawing.Point(15, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(71, 68);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Separator";
+            // 
+            // Separator_RB2
+            // 
+            this.Separator_RB2.AutoSize = true;
+            this.Separator_RB2.Location = new System.Drawing.Point(39, 13);
+            this.Separator_RB2.Name = "Separator_RB2";
+            this.Separator_RB2.Size = new System.Drawing.Size(30, 17);
+            this.Separator_RB2.TabIndex = 13;
+            this.Separator_RB2.Text = "/";
+            this.Separator_RB2.UseVisualStyleBackColor = true;
+            // 
+            // Stop_B
+            // 
+            this.Stop_B.Enabled = false;
+            this.Stop_B.Location = new System.Drawing.Point(206, 103);
+            this.Stop_B.Name = "Stop_B";
+            this.Stop_B.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Stop_B.Size = new System.Drawing.Size(127, 23);
+            this.Stop_B.TabIndex = 14;
+            this.Stop_B.Text = "Stop";
+            this.Stop_B.UseVisualStyleBackColor = true;
+            this.Stop_B.Click += new System.EventHandler(this.Stop_B_Click);
+            // 
+            // Desine_L1
+            // 
+            this.Desine_L1.AutoSize = true;
+            this.Desine_L1.Location = new System.Drawing.Point(6, 16);
+            this.Desine_L1.Name = "Desine_L1";
+            this.Desine_L1.Size = new System.Drawing.Size(31, 13);
+            this.Desine_L1.TabIndex = 15;
+            this.Desine_L1.Text = "Files:";
+            // 
+            // Desine_L2
+            // 
+            this.Desine_L2.AutoSize = true;
+            this.Desine_L2.Location = new System.Drawing.Point(6, 32);
+            this.Desine_L2.Name = "Desine_L2";
+            this.Desine_L2.Size = new System.Drawing.Size(44, 13);
+            this.Desine_L2.TabIndex = 16;
+            this.Desine_L2.Text = "Folders:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Size_L);
+            this.groupBox2.Controls.Add(this.Folders_L);
+            this.groupBox2.Controls.Add(this.Files_L);
+            this.groupBox2.Controls.Add(this.Desine_L6);
+            this.groupBox2.Controls.Add(this.Desine_L2);
+            this.groupBox2.Controls.Add(this.Desine_L1);
+            this.groupBox2.Location = new System.Drawing.Point(92, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(108, 68);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Scanned items";
+            // 
+            // Size_L
+            // 
+            this.Size_L.AutoSize = true;
+            this.Size_L.Location = new System.Drawing.Point(55, 48);
+            this.Size_L.Name = "Size_L";
+            this.Size_L.Size = new System.Drawing.Size(13, 13);
+            this.Size_L.TabIndex = 20;
+            this.Size_L.Text = "0";
+            // 
+            // Folders_L
+            // 
+            this.Folders_L.AutoSize = true;
+            this.Folders_L.Location = new System.Drawing.Point(55, 32);
+            this.Folders_L.Name = "Folders_L";
+            this.Folders_L.Size = new System.Drawing.Size(13, 13);
+            this.Folders_L.TabIndex = 18;
+            this.Folders_L.Text = "0";
+            // 
+            // Files_L
+            // 
+            this.Files_L.AutoSize = true;
+            this.Files_L.Location = new System.Drawing.Point(55, 16);
+            this.Files_L.Name = "Files_L";
+            this.Files_L.Size = new System.Drawing.Size(13, 13);
+            this.Files_L.TabIndex = 17;
+            this.Files_L.Text = "0";
+            // 
+            // Desine_L6
+            // 
+            this.Desine_L6.AutoSize = true;
+            this.Desine_L6.Location = new System.Drawing.Point(6, 48);
+            this.Desine_L6.Name = "Desine_L6";
+            this.Desine_L6.Size = new System.Drawing.Size(54, 13);
+            this.Desine_L6.TabIndex = 19;
+            this.Desine_L6.Text = "Size (Mb):";
+            // 
+            // Desine_L3
+            // 
+            this.Desine_L3.AutoSize = true;
+            this.Desine_L3.Location = new System.Drawing.Point(12, 9);
+            this.Desine_L3.Name = "Desine_L3";
+            this.Desine_L3.Size = new System.Drawing.Size(61, 13);
+            this.Desine_L3.TabIndex = 19;
+            this.Desine_L3.Text = "Scan folder";
+            // 
+            // Desine_L4
+            // 
+            this.Desine_L4.AutoSize = true;
+            this.Desine_L4.Location = new System.Drawing.Point(12, 35);
+            this.Desine_L4.Name = "Desine_L4";
+            this.Desine_L4.Size = new System.Drawing.Size(66, 13);
+            this.Desine_L4.TabIndex = 20;
+            this.Desine_L4.Text = "Result folder";
+            // 
+            // Desine_L7
+            // 
+            this.Desine_L7.AutoSize = true;
+            this.Desine_L7.Location = new System.Drawing.Point(219, 85);
+            this.Desine_L7.Name = "Desine_L7";
+            this.Desine_L7.Size = new System.Drawing.Size(33, 13);
+            this.Desine_L7.TabIndex = 21;
+            this.Desine_L7.Text = "Time:";
+            // 
+            // Time_L
+            // 
+            this.Time_L.AutoSize = true;
+            this.Time_L.Location = new System.Drawing.Point(247, 85);
+            this.Time_L.Name = "Time_L";
+            this.Time_L.Size = new System.Drawing.Size(70, 13);
+            this.Time_L.TabIndex = 21;
+            this.Time_L.Text = "00:00:00.000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 130);
+            this.Controls.Add(this.Time_L);
+            this.Controls.Add(this.Desine_L7);
+            this.Controls.Add(this.Desine_L4);
+            this.Controls.Add(this.Desine_L3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Stop_B);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BrowseOutput_B);
+            this.Controls.Add(this.BrowseInput_B);
+            this.Controls.Add(this.Start_B);
+            this.Controls.Add(this.Output_TB);
+            this.Controls.Add(this.Input_TB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Scanner 2.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_TB;
+        private System.Windows.Forms.TextBox Output_TB;
+        private System.Windows.Forms.Button Start_B;
+        private System.Windows.Forms.Button BrowseInput_B;
+        private System.Windows.Forms.Button BrowseOutput_B;
+        private System.Windows.Forms.RadioButton Separator_RB3;
+        private System.Windows.Forms.RadioButton Separator_RB4;
+        private System.Windows.Forms.RadioButton Separator_RB5;
+        private System.Windows.Forms.RadioButton Separator_RB6;
+        private System.Windows.Forms.RadioButton Separator_RB1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Separator_RB2;
+        private System.Windows.Forms.Button Stop_B;
+        private System.Windows.Forms.Label Desine_L1;
+        private System.Windows.Forms.Label Desine_L2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label Folders_L;
+        private System.Windows.Forms.Label Files_L;
+        private System.Windows.Forms.Label Desine_L3;
+        private System.Windows.Forms.Label Desine_L4;
+        private System.Windows.Forms.Label Size_L;
+        private System.Windows.Forms.Label Desine_L6;
+        private System.Windows.Forms.Label Desine_L7;
+        private System.Windows.Forms.Label Time_L;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scaner_2._0
+{
+    public partial class Form1 : Form
+    {
+        static long filesScaned = 0;
+        static long foldersScaned = 0;
+        static long sizeScaned = 0;
+        static Stopwatch timeScaned;
+        static Thread scanningThread;
+        static Thread calculationThread;
+        static StreamWriter resultFile;
+        static StreamWriter errorsFile;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_TB_TextChanged(object sender, EventArgs e)
+        {
+            var value = (Input_TB.Text);
+            if (value.Length > 0)
+                if (value[value.Length - 1] != '\\')
+                {
+                    value += @"\";
+                    Input_TB.Text = value;
+                }
+        }
+
+        private void Output_TB_TextChanged(object sender, EventArgs e)
+        {
+            var value = (Output_TB.Text);
+            if (value.Length > 0)
+                if (value[value.Length - 1] != '\\')
+                {
+                    value += @"\";
+                    Output_TB.Text = value;
+                }
+        }
+
+        private void BrowseInput_B_Click(object sender, EventArgs e)
+        {
+            using (var folder = new FolderBrowserDialog())
+                if (folder.ShowDialog() == DialogResult.OK)
+                    Input_TB.Text = folder.SelectedPath;
+        }
+
+        private void BrowseOutput_B_Click(object sender, EventArgs e)
+        {
+            using (var folder = new FolderBrowserDialog())
+                if (folder.ShowDialog() == DialogResult.OK)
+                    Output_TB.Text = folder.SelectedPath;
+        }
+
+        private void Start_B_Click(object sender, EventArgs e)
+        {
+            StartProcedure();
+        }
+
+        private void Stop_B_Click(object sender, EventArgs e)
+        {
+            StopProcedure();
+        }
+
+        private string GetSwitcherProcedure()
+        {
+            string result;
+
+            if (Separator_RB1.Checked)
+                result = "\t";
+            else if (Separator_RB2.Checked)
+                result = "/";
+            else if (Separator_RB3.Checked)
+                result = "*";
+            else if (Separator_RB4.Checked)
+                result = "\"";
+            else if (Separator_RB5.Checked)
+                result = "<";
+            else if (Separator_RB6.Checked)
+                result = ">";
+            else throw new NotImplementedException();
+
+            return result;
+        }
+
+        private void OffInterface()
+        {
+            Stop_B.Enabled = true;
+            Start_B.Enabled = false;
+            Input_TB.Enabled = false;
+            Output_TB.Enabled = false;
+            Separator_RB1.Enabled = false;
+            Separator_RB2.Enabled = false;
+            Separator_RB3.Enabled = false;
+            Separator_RB4.Enabled = false;
+            Separator_RB5.Enabled = false;
+            Separator_RB6.Enabled = false;
+            BrowseInput_B.Enabled = false;
+            BrowseOutput_B.Enabled = false;
+
+            filesScaned = 0;
+            foldersScaned = 0;
+            sizeScaned = 0;
+
+            timeScaned = new Stopwatch();
+            timeScaned.Start();
+        }
+
+        private void OnInterface()
+        {
+            Stop_B.Enabled = false;
+            Start_B.Enabled = true;
+            Input_TB.Enabled = true;
+            Output_TB.Enabled = true;
+            Separator_RB1.Enabled = true;
+            Separator_RB2.Enabled = true;
+            Separator_RB3.Enabled = true;
+            Separator_RB4.Enabled = true;
+            Separator_RB5.Enabled = true;
+            Separator_RB6.Enabled = true;
+            BrowseInput_B.Enabled = true;
+            BrowseOutput_B.Enabled = true;
+
+            timeScaned.Stop();
+            var elapsedTime = timeScaned.Elapsed;
+            string time = String.Format
+                (
+                "{0:00}:{1:00}:{2:00}.{3:000}",
+                elapsedTime.Hours,
+                elapsedTime.Minutes,
+                elapsedTime.Seconds,
+                elapsedTime.Milliseconds
+                );
+
+            Files_L.Text = filesScaned.ToString();
+            Folders_L.Text = foldersScaned.ToString();
+            Size_L.Text = (sizeScaned/1024/1024).ToString();
+            Time_L.Text = time;
+        }
+
+        public void Scanning()
+        {
+            var imputPath = Input_TB.Text;
+            var outputPath = Output_TB.Text;
+            var separator = GetSwitcherProcedure();
+
+            resultFile = new StreamWriter(outputPath + "Scanning result.txt", true, Encoding.Default, 10);
+            errorsFile = new StreamWriter(outputPath + "Scanning errors folers.txt", true, Encoding.Default, 10);
+
+            ScanFolder(imputPath, separator);
+
+            Invoke((MethodInvoker)delegate () { StopProcedure(); });
+        }
+
+        private void ScanFolder(string imputPath, string separator)
+        {
+            try
+            {
+                var allfiles = Directory.GetFiles(imputPath).ToList();
+                var allfolders = Directory.GetDirectories(imputPath).Select(x => x + "\\").ToList();
+
+                foreach (var folder in allfolders)
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(folder);
+
+                    string name = directoryInfo.Name;
+                    string size = "-";
+                    string createDate = directoryInfo.CreationTime.ToShortDateString();
+                    string createTime = directoryInfo.CreationTime.ToLongTimeString();
+                    string changeDate = directoryInfo.LastWriteTime.ToShortDateString();
+                    string changeTime = directoryInfo.LastWriteTime.ToLongTimeString();
+                    string lastOpenDate = directoryInfo.LastAccessTime.ToShortDateString();
+                    string lastOpenTime = directoryInfo.LastAccessTime.ToLongTimeString();
+                    string extension = "-";
+                    string directoryPath = folder.Remove(folder.LastIndexOf(name));
+                    
+                    resultFile.WriteLine
+                        (
+                        name + separator +
+                        size + separator +
+                        createDate + separator + 
+                        createTime + separator +
+                        changeDate + separator + 
+                        changeTime + separator +
+                        lastOpenDate + separator + 
+                        lastOpenTime + separator +
+                        extension + separator +
+                        directoryPath
+                        );
+
+                    foldersScaned++;
+                }
+
+                foreach (var files in allfiles)
+                {
+                    FileInfo fileInfo = new FileInfo(files);
+
+                    string name = fileInfo.Name;
+                    string size = fileInfo.Length.ToString();
+                    string createDate = fileInfo.CreationTime.ToShortDateString();
+                    string createTime = fileInfo.CreationTime.ToLongTimeString();
+                    string changeDate = fileInfo.LastWriteTime.ToShortDateString();
+                    string changeTime = fileInfo.LastWriteTime.ToLongTimeString();
+                    string lastOpenDate = fileInfo.LastAccessTime.ToShortDateString();
+                    string lastOpenTime = fileInfo.LastAccessTime.ToLongTimeString();
+                    string extension = fileInfo.Extension;
+                    string directoryPath = fileInfo.Directory.ToString() + "\\";
+
+                    resultFile.WriteLine
+                        (
+                        name + separator +
+                        size + separator +
+                        createDate + separator +
+                        createTime + separator +
+                        changeDate + separator +
+                        changeTime + separator +
+                        lastOpenDate + separator +
+                        lastOpenTime + separator +
+                        extension + separator +
+                        directoryPath
+                        );
+
+                    sizeScaned += fileInfo.Length;
+                    filesScaned++;
+                }
+
+                foreach (var folder in allfolders)
+                    ScanFolder(folder, separator);
+            }
+
+            catch { try { errorsFile.WriteLine(imputPath); } catch { } }
+        }
+
+        public void Calculation()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+
+                var elapsedTime = timeScaned.Elapsed;
+                string time = String.Format
+                    (
+                    "{0:00}:{1:00}:{2:00}.{3:000}",
+                    elapsedTime.Hours,
+                    elapsedTime.Minutes,
+                    elapsedTime.Seconds,
+                    elapsedTime.Milliseconds
+                    );
+
+                Invoke((MethodInvoker)delegate ()
+                {
+                    Files_L.Text = filesScaned.ToString();
+                    Folders_L.Text = foldersScaned.ToString();
+                    Size_L.Text = (sizeScaned / 1024 / 1024).ToString();
+                    Time_L.Text = time;
+                });
+            }
+        }
+
+        private void StartProcedure()
+        {
+            OffInterface();
+
+            scanningThread = new Thread(new ThreadStart(Scanning));
+            scanningThread.Start();
+
+            calculationThread = new Thread(new ThreadStart(Calculation));
+            calculationThread.Start();
+        }
+
+        private void StopProcedure()
+        {
+            try { scanningThread.Abort(); }
+            catch { MessageBox.Show("Error:\n" + "Scanning is not active. There is nothing to stop."); }
+
+            try { calculationThread.Abort(); }
+            catch { MessageBox.Show("Error:\n" + "Calculation is not active. There is nothing to stop."); }
+
+            try { resultFile.Close(); }
+            catch { MessageBox.Show("Error:\n" + "File \"Scanning result.txt\" is not opened. There is nothing to close."); }
+
+            try { errorsFile.Close(); }
+            catch { MessageBox.Show("Error:\n" + "File \"Scanning errors folers.txt\" is not opened. There is nothing to close."); }
+
+            OnInterface();
+        }
+    }
+}
